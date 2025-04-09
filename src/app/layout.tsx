4e57ad6428c8a6645b88351df450461d7543d931
Byeonghyeon <problemp123@gmail.com>
2025-04-09 19:24:00 +0900

    [feat] Provider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientProvider from "./components/Provider/ClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,8 +17,8 @@ export const metadata: Metadata = {
   title: "떠나봄",
   description: "AI 기반 여행 플래너로 여정을 떠나봄~!",
   icons: {
-    icon: '/ic.png'
-  }
+    icon: "/ic.png",
+  },
 };
 
 export default function RootLayout({
@@ -30,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
   );
